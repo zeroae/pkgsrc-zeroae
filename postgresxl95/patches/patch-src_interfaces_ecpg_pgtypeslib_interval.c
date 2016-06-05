0c177d64ab0ca7f2a6c2ededa87503199fa66573
Patrick Carlos <pcarlos@gmail.com>
2016-06-05 02:07:09 +0000

    Updated patch interfaces/ecpg/pgtypeslib/interval.c

--- postgresxl95/patches/patch-src_interfaces_ecpg_pgtypeslib_interval.c
+++ postgresxl95/patches/patch-src_interfaces_ecpg_pgtypeslib_interval.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_interfaces_ecpg_pgtypeslib_interval.c,v 1.1 2016/02/25 21:37:35 tnn Exp $
+$NetBSD$
 
---- src/interfaces/ecpg/pgtypeslib/interval.c.orig	2014-12-16 01:07:34.000000000 +0000
+--- src/interfaces/ecpg/pgtypeslib/interval.c.orig	2016-05-12 08:20:43.000000000 +0000
 +++ src/interfaces/ecpg/pgtypeslib/interval.c
 @@ -14,6 +14,10 @@
  #include "pgtypes_error.h"
@@ -12,4 +12,4 @@ $NetBSD: patch-src_interfaces_ecpg_pgtypeslib_interval.c,v 1.1 2016/02/25 21:37:
 +
  /* copy&pasted from .../src/backend/utils/adt/datetime.c */
  static int
- strtoi(const char *nptr, char **endptr, int base)
+ strtoint(const char *nptr, char **endptr, int base)
