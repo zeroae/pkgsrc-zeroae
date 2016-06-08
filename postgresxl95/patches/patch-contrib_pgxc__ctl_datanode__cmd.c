beba7c7f384714b1b1a524545c9194e7aef27828
Patrick Carlos <psodre@gmail.com>
2016-06-08 06:24:41 +0000

    Add distinfo and make it a NetBSD patch

--- postgresxl95/patches/patch-contrib_pgxc__ctl_datanode__cmd.c
+++ postgresxl95/patches/patch-contrib_pgxc__ctl_datanode__cmd.c
@@ -1,8 +1,8 @@
-diff --git a/contrib/pgxc_ctl/datanode_cmd.c b/contrib/pgxc_ctl/datanode_cmd.c
-index 109c0f5..34705f0 100644
---- a/contrib/pgxc_ctl/datanode_cmd.c
-+++ b/contrib/pgxc_ctl/datanode_cmd.c
-@@ -1263,7 +1263,12 @@ int add_datanodeMaster(char *name, char *host, int port, int pooler, char *dir,
+$NetBSD$
+
+--- contrib/pgxc_ctl/datanode_cmd.c.orig	2016-05-12 08:20:42.000000000 +0000
++++ contrib/pgxc_ctl/datanode_cmd.c
+@@ -1263,7 +1263,12 @@ int add_datanodeMaster(char *name, char
  				elog(ERROR, "ERROR: cannot connect to the coordinator master %s.\n", aval(VAR_coordNames)[ii]);
  				continue;
  			}
